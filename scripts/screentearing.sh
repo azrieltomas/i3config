4b5f98e02709b51550074ede6a9ffe8b1fc68b82
azrieltomas <azrieltomas@users.noreply.github.com>
2023-04-30 09:42:04 +1000

    Delete scripts directory

--- scripts/screentearing.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
--- scripts/torrentforever.sh
+++
@@ -1,4 +0,0 @@
-until qbittorrent; do
-    echo "Qbittorrent crashed with exit code $?.  Respawning.." >&2
-    sleep 1
-done
